065756c2da27491ddf92820bcaef679db54910dc
jacob <jacobz@yeah.net>
2015-09-16 14:44:49 +0800

    add assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,7 @@ Evbdup::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif ie_9.js form.js kobe.js kobe.css james.js james.css)
-
+ 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
