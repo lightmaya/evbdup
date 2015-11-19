1025ae59c533785ed7c92dadec06d00fdd942d1c
vv10 <261098165@qq.com>
2015-11-19 08:25:05 +0800

    select budget

---
+++ db/migrate/20151118121504_add_rule_id_to_bid_projects.rb
@@ -0,0 +1,9 @@
+class AddRuleIdToBidProjects < ActiveRecord::Migration
+  def change
+    add_column :bid_projects, :rule_id, :integer
+    add_column :bid_projects, :rule_step, :string
+    add_column :bid_projects, :budget_id, :integer
+    add_index :bid_projects, :budget_id
+    rename_column :bid_projects, :budget, :budget_money
+  end
+end
