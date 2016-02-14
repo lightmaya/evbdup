11e5e9adc98ce31d9870c1ee0796454a997912a0
dhuo14 <dhuo14@163.com>
2016-02-14 17:19:57 +0800

    banners

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,8 @@
 # -*- encoding : utf-8 -*-
 class ErrorsController < ApplicationController
-<<<<<<< HEAD
-	
-=======
+
   layout false
 
->>>>>>> 67a360d6f9308e6bb5f25ca30cd77548436e661b
   def index
 
   end
