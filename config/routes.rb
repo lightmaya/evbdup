9940ff7d8336f4854587a57145e40de58d9698c9
vv10 <261098165@qq.com>
2016-03-27 19:52:26 +0800

    mall

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Evbdup::Application.routes.draw do
   get "search", :to => 'home#search', :as => "search"
   get "more_list" => "home#more_list"
   get "dep_list" => "home#dep_list"
+  get "mall" => "mall#go_to_mall"
 
   get 'show_faqs' => "faqs#show"
 
