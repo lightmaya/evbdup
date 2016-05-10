7314be5f440f33ec74756e7f7e15a7bcd505d69f
vv10 <261098165@qq.com>
2016-05-10 14:56:30 +0800

    item dep name

--- app/models/item.rb
+++ app/models/item.rb
@@ -33,7 +33,7 @@ class Item < ActiveRecord::Base
       ItemDepartment.destroy_all(item_id: self.id, name: (all_names - names))
 
       names.each do |name|
-        dep = Department.find_by(name: name, ancestry: Dictionary.dep_supplier_id)
+        dep = Department.find_by(name: name, ancestry: Dictionary.dep_supplier_id, status: Department.effective_status)
         tmp = (dep.present? ? { name: name, department_id: dep.id } : { name: name })
         self.item_departments.find_or_create_by(tmp)
       end
